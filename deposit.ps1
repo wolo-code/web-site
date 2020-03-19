62a99ba822e8f53c397201da515c02604ca97f92
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-03-20 02:50:40 +0530

    Updated for main project location
    Main project moved to App\

--- deposit.ps1
+++ deposit.ps1
@@ -1,3 +1,3 @@
 mv public\index.html public\index_html
-copy-item -Path ".\\public\\*" -exclude "root.json","index_html",".htaccess","manifest.json",".git" -Destination "..\\..\\Project\\public" -Recurse -Force
+copy-item -Path ".\\public\\*" -exclude "root.json","index_html",".htaccess","manifest.json",".git" -Destination "..\\..\\App\\Project\\public" -Recurse -Force
 mv public\index_html public\index.html
