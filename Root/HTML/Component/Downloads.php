6b58b59585d71859393ff8d37b88267d9a029e0d
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-06-25 21:13:51 +0530

    Downloads rearranged, added unified get url

--- Root/HTML/Component/Downloads.php
+++ Root/HTML/Component/Downloads.php
@@ -2,22 +2,18 @@
 	<ul class='content-list'>
 		<li>
 			<span class='download available'>
-				<span>PWA</span>
-				<a id='download-pwa' href='/' class='content-link'>wolo.codes</a>
+				<a id='download-pwa' href='/' class='content-link'>wolo.codes</a> <span class='separator'> | </span> <span>web app</span>
 			</span>
-			<span>Web app</span>
 		</li>
 		<li>
 			<span class='download coming-soon'>
-				<span>coming soon</span>
+				<span>to be announced</span>
 				<a id='download-windows' class='content-link'><span class='image'><?php includeSVG('', 'windows'); ?></span></a>
 			</span>
-			<span>Windows store</span>
 		</li>
 		<li>
-			<span class='download download-android coming-soon'>
-				<span>coming soon</span>
-				<a class='content-link'><span class='image'><?php includeSVG('', 'Google-Play-badge'); ?></a>
+			<span class='download available download-android'>
+				<a class='content-link' href='https://play.google.com/store/apps/details?id=codes.wolo.droid'><span class='image'><?php includeSVG('', 'Google-Play-badge'); ?></a>
 			</span>
 		</li>
 		<li>
@@ -26,6 +22,9 @@
 				<a class='content-link'><span class='image'><?php includeSVG('', 'Apple-App-Store-badge'); ?></span></a>
 			</span>
 		</li>
+		<div id='download-unified-url'>
+			<span>unified url :</span> <a class='content-link' href="https://wolo.codes/get">wolo.codes/get</a>
+		</div>
 	</ul>
 </div>
 <?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
