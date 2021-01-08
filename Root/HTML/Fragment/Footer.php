305f6370cff0628e053bcbc1b332d93c8967279a
Ujjwal Singh <ujjwalsingh@outlook.com>
2021-01-08 14:54:11 +0530

    Added updating year in Copyright notice

--- Root/HTML/Fragment/Footer.php
+++ Root/HTML/Fragment/Footer.php
@@ -34,7 +34,7 @@
 		</div>
 		
 		<div class='footer-content'>
-			<a class="content-link-gray XURL" href='/license' data-target='license' data-title='License'>Copyright &copy; 2020</a>
+			<a class="content-link-gray XURL" href='/license' data-target='license' data-title='License'>Copyright &copy; <?php echo date('Y'); ?></a>
 			<a class="content-link XURL" href='/about_me' data-target='about_me' data-title='About me'>Ujjwal Singh</a><a class='content-link' target='_blank' href='/about_me' rel='author'></a>
 		</div>
 		
