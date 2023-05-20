4228fd6850dd62d0bc3b892797fa5909457e12ac
Ujjwal Singh <ujjwalsingh@outlook.com>
2023-05-20 19:43:12 +0530

    Added page: data_deletion

---
+++ Root/HTML/Component/Data_Deletion.php
@@ -0,0 +1,14 @@
+<div id='message'>
+	<h3>User can request for their data to be deleted following these steps:</h3>
+	<ol>
+		<li>email: <a class='content-link' href="mailto:delete@wolo.codes?subject=Wolo%20Code%20-%20data%20deletion%20request">delete@wolo.codes</a></li>
+		<li>with subject: <strong>Wolo Code - data deletion request</strong></li>
+		<li>from your <strong>registered</strong> email address</li>
+	</ol>
+	<ul>
+		<li>for whole account deletion - send: "<em>I request you to delete all the data associated with this email id including the account itself.</em>"</li>
+		<li>for only data deletion - send: "<em>I request you to delete only associated data with this email id, leaving the login account intact.</em>"</li>
+	</ul>
+	<p>You will recieve confirmation of the completion of your request within 3 days.</p>
+</div>
+<?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
--- Root/HTML/Fragment/Menu.php
+++ Root/HTML/Fragment/Menu.php
@@ -10,7 +10,7 @@
 			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['//wolo.codes', 'App'], ['press_release\launch', 'Launch']);
 			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['about', 'About'], ['faq', 'FAQ'], ['features', 'Features'], ['case', 'Case'], ['competition', 'Competition'], ['presentation', 'Presentation']);
 			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['downloads', 'Downloads']);
-			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['terms', 'Terms of use'], ['policy', 'Privacy policy'], ['license', 'License'], ['logo', 'Logo']);
+			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['terms', 'Terms of use'], ['policy', 'Privacy policy'], ['data_deletion', 'Data Deletion'], ['license', 'License'], ['logo', 'Logo']);
 			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['//wcodes.org', 'WCodes'], ['//wcodes.org/wordlist', 'Wordlist']);
 			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['about_me', 'About me'], ['media', 'Media']);
 ?>
