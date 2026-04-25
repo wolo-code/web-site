bd95c686cbe79787de49296d6ee91c1fdaf9a80f
Ujjwal Singh <ujjwalsingh@outlook.com>
2026-04-25 07:50:32 +0530

    updated data deletion page for Google play store compliance

--- Root/HTML/Component/Data_Deletion.php
+++ Root/HTML/Component/Data_Deletion.php
@@ -1,14 +1,34 @@
 <div id='message'>
-	<h3>User can request for their data to be deleted following these steps:</h3>
+	<h3>Data Deletion Policy</h3>
+	<p><strong>App:</strong> Wolo Code (by Wolo — wolo.codes)</p>
+	
+	<h4>Data Deleted</h4>
+	<ul>
+		<li>Account profile</li>
+		<li>Usage data</li>
+		<li>Uploaded content</li>
+	</ul>
+	
+	<h4>Deletion Timeline</h4>
+	<p>Data is permanently deleted within 7 days of request.</p>
+	
+	<h4>How to Request Data Deletion</h4>
+	<p>User can request for their data to be deleted following these steps:</p>
 	<ol>
 		<li>email: <a class='content-link' href="mailto:delete@wolo.codes?subject=Wolo%20Code%20-%20data%20deletion%20request">delete@wolo.codes</a></li>
 		<li>with subject: <strong>Wolo Code - data deletion request</strong></li>
 		<li>from your <strong>registered</strong> email address</li>
 	</ol>
+	
+	<h4>Deletion Options</h4>
 	<ul>
 		<li>for whole account deletion - send: "<em>I request you to delete all the data associated with this email id including the account itself.</em>"</li>
 		<li>for only data deletion - send: "<em>I request you to delete only associated data with this email id, leaving the login account intact.</em>"</li>
 	</ul>
-	<p>You will recieve confirmation of the completion of your request within 3 days.</p>
+	
+	<h4>Cannot Access Registered Email?</h4>
+	<p>If you cannot access your registered email, contact: <a class='content-link' href="mailto:delete@wolo.codes?subject=Wolo%20Code%20-%20data%20deletion%20request%20-%20account%20access%20issue">delete@wolo.codes</a> with proof of ownership.</p>
+	
+	<p><strong>Confirmation:</strong> You will receive confirmation of the completion of your request within 7 days.</p>
 </div>
 <?php require('../HTML/Fragment/Component_bottom_nav.php') ?>
