16b1fba5358d38bac8845d6b211113391d324107
Ujjwal Singh <ujjwalsingh@outlook.com>
2026-04-29 02:47:46 +0530

    correct copy - for oauth compliance

--- Root/HTML/Component/About.php
+++ Root/HTML/Component/About.php
@@ -7,6 +7,10 @@
 
 		<div id='location_solution_description' class='pagebreak'>
 			<h2><span class='bullet'>•</span><span>Intro</span></h2>
+			<p class='indent-b'>
+				<strong>Wolo Code</strong> is a location app by <em>Wolo</em> that converts map locations into short, word based address codes and converts those codes back into map locations.<br>
+				The app helps users search for a place, select a point on a map, generate a <em>Wolo Code</em> for that point, decode an existing <em>Wolo Code</em>, and share or save useful addresses
+			</p>
 			<?php $img_title='location_solution_description'; $ext='svg'; $extern='true'; $alt=''; $center='true'; require('Fragment/Component_image.php') ?>
 		</div>
 
@@ -47,6 +51,27 @@
 			<div id='location_app_intro' class='pagebreak'>
 				<?php $img_title='location_app_intro'; $ext='svg'; $extern='true'; $alt=''; $center='true'; require('Fragment/Component_image.php') ?>
 			</div>
+		</div>
+
+		<div id='app_homepage_summary' class='pagebreak'>
+			<h2><span class='bullet'>•</span><span>Data use</span></h2>
+			<p class='indent-b'>
+				You can use the public location tools without signing in.
+				Signing in with <em>Google</em> or <em>Facebook</em> is optional and is used only when you choose account features, such as viewing your account profile in the app or saving and opening your saved Wolo Code addresses.
+			</p>
+			<p class='indent-b'>
+				When you sign in with Google or Facebook, Wolo Code requests basic profile information such as your name and email address so the app can identify your account, show your signed-in profile, and associate saved addresses with you.
+				Wolo Code does <strong>not</strong> request account access to read your <em>Gmail</em>, <em>contacts</em>, <em>calendar</em>, <em>Drive</em> files, Facebook posts, friends list, messages, or other unrelated Google or Facebook data.
+			</p>
+			<p class='indent-b'>
+				For details on how user information is collected, used, and protected, read the
+				<a href='/policy' data-target='policy' class='content-link XURL'>Wolo Code Privacy Policy</a>.<br>
+				For details on the terms of use of the app, read the
+				<a href='/terms' data-target='terms' class='content-link XURL'>Wolo Code Terms of Use</a>.
+			</p>
+			<p class='indent-b'>
+				<small>Google and Facebook are trademarks of their respective owners. Wolo Code is not affiliated with, endorsed by, or sponsored by Google or Facebook and their parent organizations: Alphabet and Meta</small>
+			</p>
 		</div>
 
 		<div id='locaiton_appendix' class='pagebreak'>
